b6e2ce01f8d5b23e18690b2602af7d9041919347
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-21 19:24:39 -0500

    added require lines for both api and html routes

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
