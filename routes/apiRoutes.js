13fd06822456e8a9e6f6868b0cf1f73dcaed9fb3
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-21 19:41:06 -0500

    added get routes for notes and * to server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,3 +3,5 @@ const path = require("path");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+// 
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
