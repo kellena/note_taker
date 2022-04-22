84dd49bbb5be8a1ec96e1565f5ee873b26350c94
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-21 20:00:28 -0500

    shifted get/post api functionality to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,24 @@ const path = require("path");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// 
+app.get("/api/notes", function(req, res) {
+    readFileAsync(path.join(__dirname + "/db/db.json"), "utf8") 
+    .then(function(data){
+        notes=[].concat(JSON.parse(data))
+        return res.json(notes);
+    })
+});
+
+app.post("/api/notes", (req, res) => {
+    let newNotes = req.body;
+    readFileAsync(path.join(__dirname + "/db/db.json"), "utf8") 
+    .then(function (data) {
+        notes = [].concat(JSON.parse(data));
+        newNotes.id = notes.length + 1;
+        notes.push(newNotes);
+        return notes
+    }).then(function(data){
+    writeFileAsync(path.join(__dirname + "/db/db.json"), JSON.stringify(data))
+        res.json(newNotes);
+    })
+});
--- server.js
+++ server.js
@@ -16,28 +16,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get("/api/notes", function(req, res) {
-    readFileAsync(path.join(__dirname + "/db/db.json"), "utf8") 
-    .then(function(data){
-        notes=[].concat(JSON.parse(data))
-        return res.json(notes);
-    })
-});
-
-app.post("/api/notes", (req, res) => {
-    let newNotes = req.body;
-    readFileAsync(path.join(__dirname + "/db/db.json"), "utf8") 
-    .then(function (data) {
-        notes = [].concat(JSON.parse(data));
-        newNotes.id = notes.length + 1;
-        notes.push(newNotes);
-        return notes
-    }).then(function(data){
-    writeFileAsync(path.join(__dirname + "/db/db.json"), JSON.stringify(data))
-        res.json(newNotes);
-    })
-});
-
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
